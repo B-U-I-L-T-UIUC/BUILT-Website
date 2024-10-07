e2164644e19982c18453c08145b40026faa4b339
Bolden Jones <jonesbolden85@gmail.com>
2024-10-07 17:53:00 -0500

    initialized js files for home page

---
+++ src/pages/homePage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './homPage.css';
+
+function HomePage() {
+    return (
+        <div className="HomePage">
+            <header className="HomePage-header">
+                <h1>Welcome to the B[U]ILT Website!</h1>
+            </header>
+        </div>
+    );
+}
+

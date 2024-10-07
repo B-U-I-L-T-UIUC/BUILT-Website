34a4572a1e5f577d0467096203ba9a416ac5cf73
AdalPadilla <apadi24@illinois.edu>
2024-10-07 17:22:43 -0500

    !

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
     return (
         <div className="App">
             <header className="App-header">
-                <h1>Welcome to the B[U]ILT Website!</h1>
+                <h1>Welcome to the B[U]ILT Website!!</h1>
             </header>
         </div>
     );

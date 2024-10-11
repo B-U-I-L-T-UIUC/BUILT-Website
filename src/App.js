7e2eada476b187f2c641412e91276b3d7e5c6934
Bolden Jones <jonesbolden85@gmail.com>
2024-10-11 17:38:56 -0500

    footer for pages

--- src/App.js
+++ src/App.js
@@ -3,39 +3,36 @@ import HomePage from './pages/homePage';
 import AboutPage from './pages/aboutPage';
 import GetInvolvedPage from './pages/getInvolvedPage';
 import CalendarPage from './pages/calendarPage';
+import Footer from './components/footer';
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import './App.css';
 
 function App() {
     return (
         <div className="App">
-            <header className="App-header">
-
-                <Router>
+            <Router>
                     <nav>
                         <ul>
-                            <li>
-                                <Link to="/Home">Home</Link>
-                            </li>
-                            <li>
-                                <Link to="/About">About</Link>
-                            </li>
-                            <li>
-                                <Link to="/GetInvolvedPage">Get Involved</Link>
-                            </li>
-                            <li>
-                                <Link to="/Calendar">Calendar</Link>
-                            </li>
+                            <li><Link to="/Home">Home</Link></li>
+                            <li><Link to="/About">About</Link></li>
+                            <li><Link to="/GetInvolvedPage">Get Involved</Link></li>
+                            <li><Link to="/Calendar">Calendar</Link></li>
+                         
                         </ul>
                     </nav>
+              
+                <main className="App-main"> 
                     <Routes>
                         <Route path="/Home" element={<HomePage />} />
                         <Route path="/About" element={<AboutPage />} />
-                        <Route path='/GetInvolvedPage' element={<GetInvolvedPage />} />
-                        <Route path='/Calendar' element={<CalendarPage />} />
+                        <Route path="/GetInvolvedPage" element={<GetInvolvedPage />} />
+                        <Route path="/Calendar" element={<CalendarPage />} />
                     </Routes>
-                </Router>
-            </header>
+                </main>
+
+                <Footer/>
+            </Router>
+
         </div>
     );
 }
---
+++ src/components/footer.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import '../styles/footer.css'
+
+function Footer() {
+    return (
+        <footer className='footer'>
+            <div className='footer-icons'>
+                <a href='https://join.slack.com/t/builtuiuc/shared_invite/zt-2sf5zdr42-pyiMyNzisGl7f0PMa91Dhw' target='_blank' rel='nooopener noreffer'>
+                    <img src = '/slack-svgrepo-com.svg' alt='twitter svg' />
+                </a>
+                <a href='https://github.com/orgs/B-U-I-L-T-UIUC/repositories' target='_blank' rel='nooopener noreffer'>
+                    <img src = '/github-svgrepo-com.svg' alt='github svg' />
+                </a>
+                <a href='https://www.instagram.com/built_uiuc/' target='_blank' rel='nooopener noreffer'>
+                    <img src = '/instagram-svgrepo-com.svg' alt='instagram svg' />
+                </a>
+                <a href='https://www.linkedin.com/groups/14432013/' target='_blank' rel='nooopener noreffer'>
+                    <img src = '/linkedin-svgrepo-com.svg' alt='linkedin svg' />
+                </a>
+            </div>
+        </footer>
+   
+    )
+}
+
+export default Footer

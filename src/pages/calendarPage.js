e585d1d0f0b6e19e99854aa87e3b331bfaee33f8
Bolden Jones <jonesbolden85@gmail.com>
2024-10-25 03:22:00 -0500

    reformatted all pages to fit figma design, along with fixing naming converntion

--- src/pages/calendarPage.js
+++ src/pages/calendarPage.js
@@ -1,13 +1,12 @@
 import React from 'react';
+import StickyNavBar from '../components/stickyNavBar';
 import '../styles/calendarPage.css';
 
 function CalendarPage() {
     return (
-        <div className="CalendarPage">
-            <header className="CalendarPage-header">
-                <h1>Calendar</h1>
-                <p>Welcome to the calendar page!</p>
-            </header>
+        <div className="Calendar-Page">
+            <StickyNavBar />
+            <h1>Calendar</h1>
         </div>
     );
 }
--- src/pages/getInvolvedPage.js
+++ src/pages/getInvolvedPage.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import StickyNavBar from '../components/stickyNavBar'
 import '../styles/getInvolvedPage.css';
 
 function GetInvolvedPage() {
     return (
-        <div className="GetInvolved">
-            <header className="GetInvolved-header">
-                <h1>Get Involved</h1>
-                <p>Welcome to the get involved page!</p>
-            </header>
+        <div className="Get-Involved-Page">
+            <StickyNavBar />
+            <h1>Get Involved</h1>
+            <p>Welcome to the get involved page!</p>
+          
         </div>
     );
 }

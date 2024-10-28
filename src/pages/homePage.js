829cb830c9b3b5dd578f54933060fa88bb4244c4
Bolden Jones <116605562+boldenj2@users.noreply.github.com>
2024-10-28 17:29:45 -0500

    Reformatted all pages  (#36)
    * reformatted all pages to fit figma design, along with fixing naming converntion
    
    * fixed overflow issue

--- src/pages/homePage.js
+++ src/pages/homePage.js
@@ -1,14 +1,14 @@
 import React from 'react';
+import StickyNavBar from '../components/stickyNavBar';
 import '../styles/homePage.css';
 import PhotoCarousel from '../components/PhotoCarousal';
 
 function HomePage() {
     return (
-        <div className="HomePage">
-            <header className="HomePage-header">
-                <h1>Welcome to the home page!</h1>
-                <PhotoCarousel/>
-            </header>
+        <div className="Home-Page">
+            <StickyNavBar />
+            <h1>Welcome to the home page!</h1> 
+            <PhotoCarousel/>
         </div>
     );
 }

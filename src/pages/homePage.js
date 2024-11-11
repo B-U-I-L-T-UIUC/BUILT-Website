44f4a9811ad09f8643f73641fc538226b9232216
alognz <ag131@illinois.edu>
2024-11-11 17:24:35 -0600

    removed filler text

--- src/pages/homePage.js
+++ src/pages/homePage.js
@@ -1,47 +1,11 @@
 import React from 'react';
 import StickyNavBar from '../components/stickyNavBar';
 import '../styles/homePage.css';
-import HeaderTransparent from '../components/headerTransparent.js'
 
 function HomePage() {
     return (
         <div className="Home-Page">
             <StickyNavBar />
-            <h1>Welcome to the home page!</h1> 
-            <h1>Welcome to the home page!</h1> 
-            <h1>Welcome to the home page!</h1> 
-            <h1>Welcome to the home page!</h1> 
-            <h1>Welcome to the home page!</h1> 
-            <h1>Welcome to the home page!</h1> 
-            <h1>Welcome to the home page!</h1> 
-            <h1>Welcome to the home page!</h1> 
-            <h1>Welcome to the home page!</h1> 
-            <h1>Welcome to the home page!</h1> 
-            <h1>Welcome to the home page!</h1> 
-            <h1>Welcome to the home page!</h1> 
-            <h1>Welcome to the home page!</h1> 
-            <h1>Welcome to the home page!</h1> 
-            <h1>Welcome to the home page!</h1> 
-            <h1>Welcome to the home page!</h1> 
-            <h1>Welcome to the home page!</h1> 
-            <h1>Welcome to the home page!</h1> 
-            <h1>Welcome to the home page!</h1> 
-            <h1>Welcome to the home page!</h1> 
-            <h1>Welcome to the home page!</h1> 
-            <h1>Welcome to the home page!</h1> 
-            <h1>Welcome to the home page!</h1> 
-            <h1>Welcome to the home page!</h1> 
-            <h1>Welcome to the home page!</h1> 
-            <h1>Welcome to the home page!</h1> 
-            <h1>Welcome to the home page!</h1> 
-            <h1>Welcome to the home page!</h1> 
-            <h1>Welcome to the home page!</h1> 
-            <h1>Welcome to the home page!</h1> 
-            <h1>Welcome to the home page!</h1> 
-            <h1>Welcome to the home page!</h1> 
-            <h1>Welcome to the home page!</h1> 
-            <h1>Welcome to the home page!</h1> 
-            <h1>Welcome to the home page!</h1> 
             <h1>Welcome to the home page!</h1> 
         </div>
     );

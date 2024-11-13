88084a4e24480776ad94bd63312fc1c2b919e319
thali <thalia63581@icloud.com>
2024-11-13 16:27:10 -0600

    basic RBC calendar

--- src/pages/calendarPage.js
+++ src/pages/calendarPage.js
@@ -1,14 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 import StickyNavBar from '../components/stickyNavBar';
 import '../styles/calendarPage.css';
 
-function CalendarPage() {
-    return (
-        <div className="Calendar-Page">
-            <StickyNavBar />
-            <h1>Calendar</h1>
-        </div>
-    );
-}
+import { Calendar, momentLocalizer } from "react-big-calendar";
+import moment from "moment"; //NOTE: CSS import should work (big cal one)
+import "react-big-calendar/lib/css/react-big-calendar.css"; //after installing dependencies
 
+const localizer = momentLocalizer(moment);
+class CalendarPage extends Component {
+  state = {
+    events: [
+      {
+        start: moment().toDate(),
+        end: moment().add(1, "days").toDate(),
+        title: "name goes here"
+      }
+    ]
+  };
+
+  render() {
+    return (
+      <div className="Calendar-Page">
+        <StickyNavBar />
+        <h1>Calendar</h1>
+        <Calendar
+          localizer={localizer}
+          defaultDate={new Date()}
+          defaultView="month"
+          events={this.state.events}
+        /> 
+      </div> 
+    );
+  }
+}
 export default CalendarPage;

950a008422c83ee4cd869dde1209f1166f4cb283
thali <thalia63581@icloud.com>
2024-11-21 20:37:21 -0600

    created a google calendar component

--- src/pages/calendarPage.js
+++ src/pages/calendarPage.js
@@ -1,27 +1,15 @@
-import React, { Component } from 'react';
+import React from 'react';
 import StickyNavBar from '../components/stickyNavBar';
+import EmbGoogleCal from '../components/embGoogleCal';
 import '../styles/calendarPage.css';
-class CalendarPage extends Component {
-  render() {
-    return (
-      <div className = "Calendar-Page">
-        <StickyNavBar />
-        <h1>Calendar</h1>
-            
-        <div className="googleCal"> 
-            <iframe
-            src = "https://calendar.google.com/calendar/embed?src=c_8ad2baa493507a75ae60a1871af32c2947f15494ebce1d0bdec1e02cc97c2954%40group.calendar.google.com&ctz=America%2FChicago"
-            style = {{
-              border: 'solid 3px #e34103',
-              borderRadius: '7px',
-              width: '70vw',
-              height: '75vh'
-            }}
-            title = "BUILT google calendar"
-            ></iframe>
-        </div> 
-      </div>  
-    );
-  }
+
+function CalendarPage()  {
+  return (
+    <div className = "Calendar-Page">
+      <StickyNavBar />
+      <h1>Calendar</h1>
+      <EmbGoogleCal />
+    </div>  
+  );
 }
 export default CalendarPage;

f0a13421cd5d97cf1a9286e32651e3fc0ffdfea7
thali <thalia63581@icloud.com>
2024-11-22 22:26:35 -0600

    React cal + Google API starter code

--- src/pages/calendarPage.js
+++ src/pages/calendarPage.js
@@ -5,32 +5,27 @@ import { Calendar, momentLocalizer } from "react-big-calendar";
 import moment from "moment";
 import "react-big-calendar/lib/css/react-big-calendar.css"; //after installing dependencies
 import axios from 'axios';
-import ICAL from 'ical.js';  // Import ICAL.js to parse the iCal data
 
 const localizer = momentLocalizer(moment);
 
 function CalendarPage() {
     const [events, setEvents] = useState([]);
     useEffect(() => {
-        // URL of public Google Calendar iCal feed
-        //temporary development access to its proxy, need to change this for deployment of website
-        const calendarUrl = 'https://cors-anywhere.herokuapp.com/https://calendar.google.com/calendar/ical/c_8ad2baa493507a75ae60a1871af32c2947f15494ebce1d0bdec1e02cc97c2954%40group.calendar.google.com/public/basic.ics';
+        const apiKey = 'YOUR_GOOGLE_API_KEY'; //put real API key once we have it
+        const calendarId = 'c_8ad2baa493507a75ae60a1871af32c2947f15494ebce1d0bdec1e02cc97c2954@group.calendar.google.com'; // Google Calendar ID
+        const calendarUrl = `https://www.googleapis.com/calendar/v3/calendars/${calendarId}/events?key=${apiKey}`;
 
-        // Fetch the iCal data
+        // Fetch the calendar events from Google API
         axios.get(calendarUrl)
             .then(response => {
-                // Parse the iCal data
-                const jcalData = ICAL.parse(response.data);
-                const comp = new ICAL.Component(jcalData);
-                const eventsFromCalendar = comp.getAllSubcomponents('vevent');
+                const eventsFromCalendar = response.data.items; // Array of events from Google Calendar
 
-                // Map the iCal events to a format that react-big-calendar expects
+                //Map the Google Calendar events to a format that react-big-calendar expects
                 const mappedEvents = eventsFromCalendar.map(event => {
-                    const e = new ICAL.Event(event);
                     return {
-                        title: e.summary,
-                        start: e.startDate.toJSDate(),
-                        end: e.endDate.toJSDate(),
+                        title: event.summary,
+                        start: new Date(event.start.dateTime || event.start.date), // Handle date and dateTime fields
+                        end: new Date(event.end.dateTime || event.end.date),
                     };
                 });
 

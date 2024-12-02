b7f3fc966b3033c2dff2419e4c88fc59aff60400
alognz <ag131@illinois.edu>
2024-12-02 17:28:08 -0600

    brought back old nav bar

--- src/components/stickyNavBar.js
+++ src/components/stickyNavBar.js
@@ -1,13 +1,25 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import '../styles/stickyNavBar.css'; 
 
+
 const StickyNavBar = () => {
+
+  const [isScrolled, setIsScrolled] = useState(false);
+  
+  useEffect(() => {
+    const handleScroll = () => {
+      setIsScrolled(window.scrollY > 50); 
+      setIsScrolled(window.scrollY > 50);
+    };
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
 
   return (
     <>
-      <div className="navbar-wrapper">
-        <div className="navbar">
+    <div className={`navbar-wrapper ${isScrolled ? 'fixed-top' : ''}`}>
+      <div className="navbar">
           <Link to="/Home" className="button">Home</Link>
           <Link to="/About" className="button">About Us</Link>
           <Link to="/Calendar" className="button">Calendar</Link>

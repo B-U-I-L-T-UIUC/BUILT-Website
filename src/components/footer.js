549259372fec8ae3dcf83cb1ae183e79213749c4
Bolden Jones <jonesbolden85@gmail.com>
2024-12-01 23:03:11 -0600

    more unneeded slashes

--- src/components/footer.js
+++ src/components/footer.js
@@ -6,16 +6,16 @@ function Footer() {
         <footer className='footer'>
             <div className='footer-icons'>
                 <a href='https://join.slack.com/t/builtuiuc/shared_invite/zt-2sf5zdr42-pyiMyNzisGl7f0PMa91Dhw' target='_blank' rel='noopener noreferrer'>
-                    <img src = '/slack-svgrepo-com.svg' alt='twitter svg' />
+                    <img src = 'slack-svgrepo-com.svg' alt='twitter svg' />
                 </a>
                 <a href='https://github.com/orgs/B-U-I-L-T-UIUC/repositories' target='_blank' rel='noopener noreferrer'>
-                    <img src = '/github-svgrepo-com.svg' alt='github svg' />
+                    <img src = 'github-svgrepo-com.svg' alt='github svg' />
                 </a>
                 <a href='https://www.instagram.com/built_uiuc/' target='_blank' rel='noopener noreferrer'>
-                    <img src = '/instagram-svgrepo-com.svg' alt='instagram svg' />
+                    <img src = 'instagram-svgrepo-com.svg' alt='instagram svg' />
                 </a>
                 <a href='https://www.linkedin.com/in/built-uiuc/' target='_blank' rel='noopener noreferrer'>
-                    <img src = '/linkedin-svgrepo-com.svg' alt='linkedin svg' />
+                    <img src = 'linkedin-svgrepo-com.svg' alt='linkedin svg' />
                 </a>
             </div>
         </footer>

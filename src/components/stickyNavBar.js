ade4a23d78dce8ab31ca9c7b0712ecde2218810e
alognz <ag131@illinois.edu>
2024-12-02 18:07:07 -0600

    added hamburger menu for smaller screens

--- src/components/stickyNavBar.js
+++ src/components/stickyNavBar.js
@@ -6,7 +6,9 @@ import '../styles/stickyNavBar.css';
 const StickyNavBar = () => {
 
   const [isScrolled, setIsScrolled] = useState(false);
-  
+  const [menuOpen, setMenuOpen] = useState(false);
+
+
   useEffect(() => {
     const handleScroll = () => {
       setIsScrolled(window.scrollY > 20); 
@@ -15,14 +17,27 @@ const StickyNavBar = () => {
     return () => window.removeEventListener('scroll', handleScroll);
   }, []);
 
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <>
-    <div className={`navbar-wrapper ${isScrolled ? 'fixed-top' : ''}`}>
-      <div className="navbar">
-          <Link to="/Home" className="button">Home</Link>
-          <Link to="/About" className="button">About Us</Link>
-          <Link to="/Calendar" className="button">Calendar</Link>
-          <Link to="/Get-Involved" className="button">Get Involved</Link>
+      <div className={`navbar-wrapper ${isScrolled ? 'fixed-top' : ''}`}>
+        <div className="navbar">
+          <button className="hamburger-menu" onClick={toggleMenu}>
+            {/* Hamburger Icon */}
+            <span className="line"></span>
+            <span className="line"></span>
+            <span className="line"></span>
+          </button>
+          <div className={`nav-links ${menuOpen ? 'show' : ''}`}>
+            {/* Links for both the regular and hamburger menus */}
+            <Link to="/Home" className="button">Home</Link>
+            <Link to="/About" className="button">About Us</Link>
+            <Link to="/Calendar" className="button">Calendar</Link>
+            <Link to="/Get-Involved" className="button">Get Involved</Link>
+          </div>
         </div>
       </div>
     </>

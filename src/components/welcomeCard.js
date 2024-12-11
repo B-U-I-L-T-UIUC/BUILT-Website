c36177869fc6d87ac977cb2d3e4ccae45dcaa562
Bolden Jones <jonesbolden85@gmail.com>
2024-12-11 12:13:00 -0600

    eslint

--- src/components/welcomeCard.js
+++ src/components/welcomeCard.js
@@ -11,16 +11,16 @@ function WelcomeCard({name, imageSrc, description}) {
                 join us in building a diverse future!
             </p>  
             <div className='icons'>
-                <a href='https://join.slack.com/t/builtuiuc/shared_invite/zt-2sf5zdr42-pyiMyNzisGl7f0PMa91Dhw' target='_blank' rel='nooopener noreffer'>
+                <a href='https://join.slack.com/t/builtuiuc/shared_invite/zt-2sf5zdr42-pyiMyNzisGl7f0PMa91Dhw' target='_blank' rel='noopener noreferrer'>
                     <img src = '/slack-svgrepo-com.svg' alt='twitter svg' />
                 </a>
-                <a href='https://github.com/orgs/B-U-I-L-T-UIUC/repositories' target='_blank' rel='nooopener noreffer'>
+                <a href='https://github.com/orgs/B-U-I-L-T-UIUC/repositories' target='_blank' rel='noopener noreferrer'>
                     <img src = '/github-svgrepo-com.svg' alt='github svg' />
                 </a>
-                <a href='https://www.instagram.com/built_uiuc/' target='_blank' rel='nooopener noreffer'>
+                <a href='https://www.instagram.com/built_uiuc/' target='_blank' rel='noopener noreferrer'>
                     <img src = '/instagram-svgrepo-com.svg' alt='instagram svg' />
                 </a>
-                <a href='https://www.linkedin.com/in/built-uiuc/' target='_blank' rel='nooopener noreffer'>
+                <a href='https://www.linkedin.com/in/built-uiuc/' target='_blank' rel='noopener noreferrer'>
                     <img src = '/linkedin-svgrepo-com.svg' alt='linkedin svg' />
                 </a>
             </div>     
